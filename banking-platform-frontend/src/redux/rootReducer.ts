1fb449af107ef86dafbda770032fc99f4b9c9558
Srikanta kumar gouda <srikanta0004@gmail.com>
2025-07-16 11:20:08 +0530

    Reapply "api integration for login api with fundSelection"
    This reverts commit e2574879bdb57fbfb69a540d5ffedce1445d7066.

--- banking-platform-frontend/src/redux/rootReducer.ts
+++ banking-platform-frontend/src/redux/rootReducer.ts
@@ -2,13 +2,18 @@ import { combineReducers } from '@reduxjs/toolkit';
 import portfolioSlice from '../slices/portfolioSlice';
 import transactionSlice from '../slices/transactionSlice';
 import loginSlice from '../slices/loginSlice';
+import fundSlice from '../slices/fundSlice';
+import fetchFundByIdSlice from '../slices/fetchFundByIdSlice';
 // import registerReducer from '../redux/registerSlice';
 
 const rootReducer = combineReducers({
 //   register: registerReducer
     portfolio:portfolioSlice,
     transaction:transactionSlice,
-    login:loginSlice
+    login:loginSlice,
+    fund:fundSlice,
+    fetchFundById:fetchFundByIdSlice
+
 
   
 });
